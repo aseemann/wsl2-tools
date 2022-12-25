4cce8475fd558676ce187a73bc28b6f93be54052
Axel Seemann <kummeraxel@gmail.com>
2022-12-25 19:33:21 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+#echo -e "Init WSL2\n"
+
+if [ ! -d /run/screen ]; then
+    echo -e "Create screen session directory"
+    mkdir -p -m 0777 /run/screen
+fi
+
+if [ ! -S /var/run/docker.sock ]; then
+    echo -e "Start docker"
+    mkdir -p /sys/fs/cgroup/systemd || :
+    mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd || :
+    /etc/init.d/docker start
+fi
+
+if [ -f /var/run/crond.pid ]; then
+    cron
+fi
+
+#echo -e "WSL2 initialized\n"
